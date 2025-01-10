4a5527ebfa6d3813daa29b7004a40b18f3f4426e
James <Jamestngo02@gmail.com>
2025-01-10 16:06:03 -0500

    added button

--- roaminate/src/App.tsx
+++ roaminate/src/App.tsx
@@ -1,10 +1,15 @@
 function App() {
   let name = "James";
 
+  const buttonCLick = () => {
+    console.log("clicked");
+  };
+
   return (
     <>
       <h1>hello world {name} </h1>
       <p>testing the develop branch</p>
+      <button onClick={buttonCLick}>Click me</button>
     </>
   );
 }
